388bcee0a54b1825db48410cc6c8e2841b6395f8
23069cr <23069cr@hvhs.school.nz>
2025-06-03 23:50:37 +0000

    Jun 3, 2025, 11:50 PM

--- Movies.js
+++ Movies.js
@@ -1,9 +1,12 @@
 function start() {
  let userMovie = prompt("What movie do you need")
  }
- if (userMovie == Femboys){
+ if (userMovie == "help"){
   prompt("Bro you need to get a life")
- }else{
+ }
+ else{
   alert("You are now renting " + userMovie)
  }
+function end() {
+  alert("Have a great movie experience!")
 }
