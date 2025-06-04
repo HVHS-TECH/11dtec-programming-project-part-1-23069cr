8bd08afc772dec81146aa11cbb79634ba35a9203
23069cr <23069cr@hvhs.school.nz>
2025-06-04 00:01:39 +0000

    Jun 4, 2025, 12:01 AM

--- Movies.js
+++ Movies.js
@@ -1,9 +1,7 @@
 function start() {
   var userMovie = prompt("What movie do you need")
-  if (userMovie == null){
-    alert("You must enter in a movie name")
-    return; 
-  } else {
-    alert("You are now renting " + userMovie);
+  while (userMovie == null){
+    userMovie = prompt("What movie do you need");
   }
+  alert("You are now renting " + userMovie);
 }
